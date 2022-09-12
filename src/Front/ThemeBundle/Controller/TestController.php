dea4845ebc7e340f5f7113f997bee2ce4b8220f2
Amine <bensaadat.amine@gmail.com>
2022-09-12 16:48:41 +0100

    update template of front

--- src/Front/ThemeBundle/Controller/TestController.php
+++ src/Front/ThemeBundle/Controller/TestController.php
@@ -11,8 +11,6 @@ class TestController extends AbstractController{
      * @Route("/testFrontTheme", name="front_theme")
      */
     public function testTheme(){
-        return $this->render('@FrontThemeBundle/index.html.twig');
-        dump("ee");
-        die;
+        return $this->render('@FrontThemeBundle/base_home.html.twig');
     }    
 }
