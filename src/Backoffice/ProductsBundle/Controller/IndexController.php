8b07f075eb7461d9f175a9de130682d344b9b9fb
Amine <bensaadat.amine@gmail.com>
2022-09-14 11:58:04 +0100

    add product progress

--- src/Backoffice/ProductsBundle/Controller/IndexController.php
+++ src/Backoffice/ProductsBundle/Controller/IndexController.php
@@ -2,10 +2,34 @@
 
 namespace App\Backoffice\ProductsBundle\Controller;
 
+use App\Backoffice\UserBundle\Form\ProductFormType;
+use App\Entity\Products;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController  extends AbstractController {
     public function index(){
         return $this->render('@BoProductsBundle/index.html.twig');
+    }
+
+    public function addProduct(Request $request, EntityManagerInterface $em){
+        $products = new Products();
+        $productForm = $this->createForm(ProductFormType::class, $products);
+        $productForm->handleRequest($request);
+        
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            // save the datat
+            $products->setName($productForm->get('name')->getData());
+           
+            $products->setQty(12);
+            $em->persist($products);
+            $em->flush();
+            dump($productForm, $productForm->get('name')->getData());
+            die;
+        }
+        
+        return $this->render('@BoProductsBundle/add_product.html.twig', [
+            'productForm' => $productForm->createView()]);
     }
 }
---
+++ src/Backoffice/UserBundle/Form/ProductFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Backoffice\UserBundle\Form;
+
+use App\Entity\Products;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ProductFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => [        
+                    'class' => 'form-control',
+                    'placeholder' => 'product_name'
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
